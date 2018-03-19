5d2a4ca59a0d5b61cd503706beb9fff9118f2d7d
gityug <yug@hotmail.com>
2018-03-18 18:51:00 -0700

    Update find-all-numbers-disappeared-in-an-array.cc

--- samples/c++/find-all-numbers-disappeared-in-an-array.cc
+++ samples/c++/find-all-numbers-disappeared-in-an-array.cc
@@ -4,13 +4,13 @@ public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         
         for (int i = 0; i < nums.size(); ++i) {            
-            if (i != nums[i]-1)
+            if (nums[i]-1 != i)
                 while (nums[i] != nums[nums[i]-1])
                     swap(nums[i], nums[nums[i]-1]);
         }
         vector<int> r;
         for (int i = 0; i < nums.size(); ++i) {
-            if (i != (nums[i]-1)) r.push_back(i+1);
+            if (nums[i]-1 != i) r.push_back(i+1);
         }
         return r;
     }
