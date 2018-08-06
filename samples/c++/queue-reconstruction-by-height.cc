1ebc0064b7576bc20588675edc568c3b189dc9ea
gityug <yug@hotmail.com>
2018-08-06 13:16:42 -0700

    Update queue-reconstruction-by-height.cc

--- samples/c++/queue-reconstruction-by-height.cc
+++ samples/c++/queue-reconstruction-by-height.cc
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/queue-reconstruction-by-height/
+// Hint: http://massivealgorithms.blogspot.com/2016/09/leetcode-406-queue-reconstruction-by.html
+// Elegant idea: https://leetcode.com/problems/queue-reconstruction-by-height/discuss/89437/O(n2)-solution-with-O(n)-extra-space
+// Hint: https://leetcode.com/problems/queue-reconstruction-by-height/discuss/89431/C++-another-O(n2)-approach-sorting-k-instead-of-h
 class Solution {
     static bool cmp(pair<int, int>& a, pair<int, int>& b) {
         return (a.second < b.second || (a.second == b.second && a.first < b.first));
